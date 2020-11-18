2b194579199a80d23ca7b6b9e7011efc5733bf28
LisandroGuerra <lisandro.digital@gmail.com>
2020-11-17 21:58:17 -0300

    coment daysDiff.php

--- dateTransactions/daysDiff.php
+++ dateTransactions/daysDiff.php
@@ -1,5 +1,6 @@
 <?php
 function days_diff($date){
+    //test to remember the format of $date
     $today = strtotime(date("Y-m-d"));
     $date = strtotime($date);
     $days_diff = (int)floor(($today - $date)/(60*60*24));
